32138c112f8e9cc6b0ac765cb2a2067bea126453
Timur Meyster <timur.meyster@gmail.com>
2015-01-08 12:54:21 -0800

    Added authorization for Music App User and Session

---
+++ week4/w4d4/Music_app/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MusicApp::Application.config.secret_key_base = 'd88b9b5cbd762b78f086f3862ea5f804e28657835b4ad25b455c3365b74a5063dfbf5624a3c2b98ea13d099ee2147fb71428c62c4d7ee02e8ef593ea93772251'
---
+++ week4/w4d4/Music_app/config/routes.rb
@@ -0,0 +1,4 @@
+MusicApp::Application.routes.draw do
+  resources :users, only: [:new, :create, :show]
+  resource :session, only: [:new, :create, :destroy]
+end
